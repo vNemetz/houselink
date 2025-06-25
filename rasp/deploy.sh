4147344480239fd41f60e5ad54f85f0f68756bf4
vNemetz <vininemetz@gmail.com>
2025-06-25 20:02:32 -0300

    working version

--- rasp/deploy.sh
+++ rasp/deploy.sh
@@ -3,9 +3,9 @@
 echo "Deploying files to Raspberry Pi..."
 
 # Copy files to Raspberry Pi
-scp /home/vnemetz/Documentos/site/rasp/servo_controller.py rasp@192.168.198.84:/home/rasp/servo/
-scp /home/vnemetz/Documentos/site/rasp/requirements.txt rasp@192.168.198.84:/home/rasp/servo/
-scp /home/vnemetz/Documentos/site/rasp/servo-controller.service rasp@192.168.198.84:/home/rasp/servo/
+scp /home/vnemetz/Documentos/oficinas41/rasp/servo_controller.py rasp@192.168.198.84:/home/rasp/servo/
+scp /home/vnemetz/Documentos/oficinas41/rasp/requirements.txt rasp@192.168.198.84:/home/rasp/servo/
+scp /home/vnemetz/Documentos/oficinas41/rasp/servo-controller.service rasp@192.168.198.84:/home/rasp/servo/
 
 # SSH into Raspberry Pi and update the service
 ssh rasp@192.168.198.84 << 'EOF'
